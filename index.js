0333f55121319186c1bafc9a1700e4feaf12463e
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-05-24 16:14:53 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+let mainWindow
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
+if (!app.isDefaultProtocolClient('electron-demonstration'))
+    app.setAsDefaultProtocolClient('electron-demonstration')
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 1200, height: 800, minWidth: 300, minHeight: 300, frame: false })
+  mainWindow.loadFile('./client/index.html')
+  mainWindow.maximize()
+  mainWindow.on('closed', () => mainWindow = null)
+  
+  if (process.env.DEV) mainWindow.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => process.platform !== 'darwin' ? app.quit() : '')
+app.on('activate', () => mainWindow === null ? createWindow() : '')
